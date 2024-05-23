86664c0dc8fafcb8071200904b5f8d86f493d10d
Reon Fernandes <159623954+FernandesReon@users.noreply.github.com>
2024-05-23 12:00:13 +0530

    Update App.java
    called the function chooseDifficulty using class instance.

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         GuessGame game = new GuessGame();
-        game.guessNumber();
+        game.chooseDifficulty();
     }
 }
